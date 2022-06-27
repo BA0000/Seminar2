4d4015681d271acf4916165b2d508bcae5d06619
Gunter <kaiz@yandex.ru>
2022-06-27 17:04:26 +0300

    on lection

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int Znach3()
+{
+    int rand = new Random().next(100, 1000);
+    Console.WriteLine("rand number is " + rand);
+    int FirstNumber = rand / 100;
+    int LustNumber = rand % 10;
+    int result = FirstNumber * 10 + LustNumber;
+    Console.WriteLine(result);
+    return result;
+}
+
+Znach3();
