3fb776f976ae31473dc457660b638fcfadd921e5
Gunter <kaiz@yandex.ru>
2022-06-28 20:32:35 +0300

    first mission

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
-﻿int Znach3()
-{
-    int rand = new Random().next(100, 1000);
-    Console.WriteLine("rand number is " + rand);
-    int FirstNumber = rand / 100;
-    int LustNumber = rand % 10;
-    int result = FirstNumber * 10 + LustNumber;
-    Console.WriteLine(result);
-    return result;
-}
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Znach3();
+
+// int number;
+// int SecondNumber;
+
+// Console.WriteLine("Enter number: ");
+// number = Convert.ToInt32(Console.ReadLine());
+
+// SecondNumber = (number / 10) % 10;
+
+// Console.WriteLine("Second number is " + SecondNumber);
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+void ThirdNum(int a)
+{
+    
+}
